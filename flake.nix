5cbf183c7195d40e141e4692eb113e2f76cccb0f
ReplayCoding <replaycoding@gmail.com>
2022-09-11 13:18:37 -0700

    fenix

--- flake.nix
+++ flake.nix
@@ -1,54 +1,35 @@
 {
   inputs = {
-    naersk.url = "github:nix-community/naersk/master";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    utils.url = "github:numtide/flake-utils";
     fenix = {
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    flake-utils.url = "github:numtide/flake-utils";
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
   };
 
   outputs = {
     self,
-    nixpkgs,
-    utils,
-    naersk,
     fenix,
+    flake-utils,
+    naersk,
+    nixpkgs,
   }:
-    utils.lib.eachDefaultSystem (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-      rust = with fenix.packages.${system}; rec {
-        native = latest;
-        dev.toolchain = combine [native.toolchain rust-analyzer];
-      };
-    in {
-      defaultPackage = naersk-lib.buildPackage ./.;
-
-      defaultApp = utils.lib.mkApp {
-        drv = self.defaultPackage."${system}";
-      };
-
-      devShell = with pkgs;
-        mkShell {
-          nativeBuildInputs = [
-            rust.dev.toolchain
-            mold
-            cmake
-          ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-
-          LD_LIBRARY_PATH =
-            nixpkgs.lib.strings.makeLibraryPath
-            (with pkgs; [
-              xorg.libX11
-              xorg.libXcursor
-              # xorg.libXrandr
-              libxkbcommon
-            ]);
-
-          MOLD_PATH = "${pkgs.mold}/bin/mold";
-          LD_PRELOAD = "${pkgs.mold}/lib/mold/mold-wrapper.so";
+    flake-utils.lib.eachDefaultSystem (system: {
+      packages.default = let
+        pkgs = nixpkgs.legacyPackages.${system};
+        toolchain = fenix.packages.${system}.minimal.toolchain;
+      in
+        (naersk.lib.${system}.override {
+          cargo = toolchain;
+          rustc = toolchain;
+        })
+        .buildPackage {
+          src = ./.;
         };
     });
 }
