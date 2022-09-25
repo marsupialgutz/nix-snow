dc1696c72e0d6d7db6186742a00005d539d3249f
marsupialgutz <mars@possums.xyz>
2022-09-24 22:46:46 -0400

    uhrm

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-#![feature(let_chains)]
 mod modes;
-
 use {
+    bpaf::Bpaf,
     json::parse,
     modes::{add::add_package, remove::remove_package},
     once_cell::sync::Lazy,
     serde_derive::Deserialize,
+    spinoff::{Color, Spinner, Spinners},
     std::{
-        env::{args, var},
+        env::var,
         fs::read_to_string,
         io::Write,
         process::{exit, Command, Stdio},
@@ -25,25 +25,28 @@ pub struct Config {
 
 pub static CONFIG: Lazy<Config> = Lazy::new(read_config);
 
-fn main() {
-    let args = args().collect::<Vec<_>>();
+#[derive(Clone, Debug, Bpaf)]
+#[bpaf(options, version)]
+struct Args {
+    #[bpaf(short('d'))]
+    dry_run: bool,
+    #[bpaf(short('a'))]
+    add: Option<String>,
+    #[bpaf(short('r'))]
+    remove: Option<String>,
+}
 
-    match args.len() {
-        1 => {
-            eprintln!("Usage: snow [-d/--dry-run] [add/remove] <package>");
-            exit(1);
-        }
-        2 => {
-            eprintln!("Please enter a package name.");
-            exit(1);
-        }
-        3 | 4 => {
-            run(args);
-        }
-        _ => {
-            eprintln!("Too many arguments.");
-            exit(1);
-        }
+fn main() {
+    let opts = args().run();
+
+    if opts.dry_run {
+        println!("dry run");
+    }
+
+    if let Some(add) = opts.add {
+        println!("Adding {add}");
+    } else if let Some(remove) = opts.remove {
+        println!("Removing {remove}");
     }
 }
 
@@ -57,132 +60,4 @@ fn read_config() -> Config {
     })
     .unwrap();
     from_str(&content).unwrap()
-}
-
-fn run(args: Vec<String>) {
-    let mut output_name = String::new();
-    let mut output_new = Vec::new();
-
-    let command = Command::new("nix")
-        .args(&["search", "--json", "nixpkgs", &args[2]])
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .spawn()
-        .unwrap();
-
-    let binding = command.wait_with_output().expect("Failed to wait on sed");
-    let output = String::from_utf8_lossy(binding.stdout.as_slice());
-    let parsed = parse(&output).unwrap();
-
-    let mut packages = Vec::new();
-    for (key, _) in parsed.entries() {
-        packages.push(key.replacen("legacyPackages.x86_64-linux.", "", 1));
-    }
-
-    if packages.len() == 1 {
-        output_name = String::from_utf8_lossy(packages[0].as_bytes()).to_string();
-    }
-
-    let file = with_contents(output.as_bytes());
-
-    if packages.len() > 1 {
-        let mut fzf = Command::new("fzf")
-            .args(&[
-                "--preview-window=wrap:45",
-                "--preview",
-                format!(
-                    r#"cat {} | jq -rcs '.[0]["legacyPackages.x86_64-linux.{{}}"]["description"]'"#,
-                    file.path().display()
-                )
-                .as_str(),
-            ])
-            .stdin(Stdio::piped())
-            .stdout(Stdio::piped())
-            .spawn()
-            .unwrap_or_else(|e| panic!("Failed to start fzf, error: {e}"));
-        let stdin = fzf.stdin.as_mut().unwrap();
-
-        stdin.write_all(packages.join("\n").as_bytes()).unwrap();
-
-        output_new = fzf
-            .wait_with_output()
-            .expect("Failed to wait on fzf")
-            .stdout;
-
-        if output_new.is_empty() {
-            eprintln!("No package selected. Exiting...");
-            exit(1);
-        }
-    }
-
-    let home_file = read_to_string({
-        if let Some(path) = &CONFIG.path {
-            path.replace("~", &var("HOME").unwrap())
-        } else {
-            format!("{}/nix-config/home/default.nix", var("HOME").unwrap())
-        }
-    })
-    .unwrap()
-    .split('\n')
-    .map(|x| x.to_string())
-    .collect::<Vec<_>>();
-
-    if let Some(beginning) = home_file.iter().position(|x| x.trim().contains("# SNOW BEGIN")) && let Some(end) = home_file.iter().position(|x| x.trim().contains("# SNOW END")) {
-        let output_as_string = from_utf8(output_name.as_bytes()).unwrap().to_owned();
-        let output_new_as_string = from_utf8(&output_new).unwrap().to_owned();
-
-        if (packages.len() == 1 && output_as_string.trim().is_empty()) || (packages.len() > 1 && output_new_as_string.trim().is_empty()) {
-            eprintln!("Package {} not found.", &args[2]);
-            exit(1);
-        }
-
-        match args[1].as_str() {
-            "--help" => {
-                eprintln!("Usage: snow [add/remove] <package_name>");
-                exit(0);
-            }
-            "a" | "add" => {
-                add_package(
-                    home_file,
-                    beginning,
-                    end,
-                    packages,
-                    output_as_string,
-                    output_new_as_string,
-                    {
-                        if args[3] == "-d" || args[3] == "--dry-run" {
-                            true
-                        } else {
-                            false
-                        }
-                    }
-                );
-            }
-            "r" | "remove" => {
-                remove_package(
-                    home_file,
-                    beginning,
-                    end,
-                    packages,
-                    output_as_string,
-                    output_new_as_string,
-                    {
-                        if args[3] == "-d" || args[3] == "--dry-run" {
-                            true
-                        } else {
-                            false
-                        }
-                    }
-
-                );
-            }
-            _ => {
-                eprintln!("Please enter a valid command.");
-                exit(1);
-            }
-        }
-    } else {
-        eprintln!("Begin/End location not found.");
-        exit(1);
-    }
 }
